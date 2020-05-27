caab417239d06fd9506196b5347466b88650ac9c
Bilgihan Kose <bilgihankose@gmail.com>
2020-05-27 16:02:22 +0300

    initial commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Bilgihan Köse on 27.05.2020.
+//  Copyright © 2020 Bilgihan Kose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
